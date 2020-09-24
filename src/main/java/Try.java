1596504343239425f974f32db7db555f29ecc184
ginfin <gara_0100@mail.ru>
2020-09-24 11:15:35 +0300

    fix pom file

--- src/main/java/Try.java
+++ src/main/java/Try.java
@@ -1,17 +1,25 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Scanner;
 
 public class Try {
-    public static void main(String[] args) throws Exception  {
 
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        String sureName = scan.nextLine();
+        String group = scan.nextLine();
+        String doxsPath = scan.nextLine();
         //Blank Document
         //
         XWPFDocument document = new XWPFDocument();
 
         //Write the Document in file system
-        FileOutputStream out = new FileOutputStream( new File("createdocument.docx"));
+        PDDocument doc = PDDocument.load(new File(doxsPath));
         document.write(out);
         out.close();
         System.out.println("createdocument.docx written successully");
