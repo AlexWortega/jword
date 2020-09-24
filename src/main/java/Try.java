b63ea80abeac306d314a18ab7204837af8b3188f
ginfin <gara_0100@mail.ru>
2020-09-24 12:16:53 +0300

    dotn arba soft

--- src/main/java/Try.java
+++ src/main/java/Try.java
@@ -1,8 +1,12 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Try {
@@ -10,6 +14,11 @@ public class Try {
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
+        FileReader reader = new FileReader("notes3.txt");
+        Scanner scan1 = new Scanner(reader);
+        String code = "";
+
+        int i = 725;
         String name = scan.nextLine();
         String sureName = scan.nextLine();
         String group = scan.nextLine();
@@ -20,8 +29,27 @@ public class Try {
 
         //Write the Document in file system
         PDDocument doc = PDDocument.load(new File(doxsPath));
-        document.write(out);
-        out.close();
-        System.out.println("createdocument.docx written successully");
+        PDPage page = doc.getPage(0);
+        PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+        contentStream.beginText();
+        contentStream.setFont(PDType1Font.TIMES_ROMAN, 16);
+
+        while (scan1.hasNextLine()) {
+
+            code = scan1.nextLine();
+            contentStream.showText(code);
+            contentStream.newLineAtOffset(25,i);
+            i++;
+            contentStream.newLine();
+
+
+        }
+
+        contentStream.endText();
+        System.out.println("Content added");
+        contentStream.close();
+        doc.save(new File("12345.pdf"));
+        doc.close();
+        System.out.println("createdocument.pdf written successully");
     }
 }
